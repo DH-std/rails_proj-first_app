9d36b643d4d6c9caa1448c106922db43b41f14e0
dili <dili.hu@atomicobject.com>
2014-05-15 10:39:41 -0400

    set ups

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,22 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-FirstSpp::Application.config.secret_key_base = '7212f56ab2b160d99f46e3bd89a70c2a00464f0f829d2c87d34da01f56cc0ba0332b4f81bd6ff7d2f4f42ce61eca328e31f1f7987322c42c7d0a77890908279c'
+
+require 'securerandom'
+
+def secure_token
+	token_life = Rails.root.join('.secret')
+	if File.exist?(token_life)
+		# Use the existing token.
+    	File.read(token_file).chomp
+ 	else
+    	# Generate a new token and store it in token_file.
+    	token = SecureRandom.hex(64)
+    	File.write(token_file, token)
+    	token
+  	end
+end
+
+FirstSpp::Application.config.secret_key_base = secure_token
+
+#FirstSpp::Application.config.secret_key_base = '7212f56ab2b160d99f46e3bd89a70c2a00464f0f829d2c87d34da01f56cc0ba0332b4f81bd6ff7d2f4f42ce61eca328e31f1f7987322c42c7d0a77890908279c'
